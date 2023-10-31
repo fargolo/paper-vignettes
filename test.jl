b03da050e5f0e02a66902105f5e55fa8efa743d8
fargolo <felipe.c.argolo@protonmail.com>
2023-10-31 20:32:26 -0300

    Distributed version

--- test.jl
+++ test.jl
@@ -5,7 +5,7 @@ include("src/SymbolicInference.jl")
 # Hyperparams
 my_seed = 1234
 cycles = 8
-sd_norm = 3
+sd_norm = 1
 rec_rate = 0.5
 
 rng = MersenneTwister(my_seed)
@@ -14,19 +14,35 @@ time_series_sin = map(sin,0:0.3:2*pi*cycles)
 n_sample=length(time_series_sin)
 dist = Normal(0,sd_norm)
 time_series = rand(dist,n_sample)
+time_series_sin_nois = time_series .+ time_series_sin
+p_tsn = plot(1:length(time_series_sin_nois),time_series_sin_nois)
+p_sin = plot(1:length(time_series_sin_nois),time_series_sin)
+p_r = plot(1:length(time_series_sin_nois),time_series)
+plot(p_tsn,p_sin,p_r,layout=(3,1))
+res_recs = map(x -> RecurrenceAnalysis.RecurrenceMatrix(x,rec_rate;fixedrate=true),
+        [time_series_sin_nois,time_series_sin,time_series])
+        res_infs = map(x -> double_inference(x,rec_rate;fixedrate=true),res_recs)
 
-time_series = RecurrenceAnalysis.RecurrenceMatrix(time_series,rec_rate;fixedrate=true)
+
+time_series_sin_nois = RecurrenceAnalysis.RecurrenceMatrix(time_series_sin_nois,rec_rate;fixedrate=true)
 time_series_sin = RecurrenceAnalysis.RecurrenceMatrix(time_series_sin,rec_rate;fixedrate=true)
+time_series = RecurrenceAnalysis.RecurrenceMatrix(time_series,rec_rate;fixedrate=true)
 
-results = map(x -> double_inference(time_series_sin;seqs=x),["double","poincare","recurrences"])
+
+results_sin_r = map(x -> double_inference(time_series_sin_nois;seqs=x),["double","poincare","recurrences"])
+results_sin = map(x -> double_inference(time_series_sin;seqs=x),["double","poincare","recurrences"])
 results_r = map(x -> double_inference(time_series;seqs=x),["double","poincare","recurrences"])
 
-ps = map(plot,results)
+
+ps_sin_r = map(plot,results_sin_r)
+ps_sin = map(plot,results_sin)
 ps_r = map(plot,results_r)
 
-p_sin = plot(ps[1],ps[2],ps[3],layout=(3,1);ylims = (0, 0.3),title="sin(x)")
-p_r = plot(ps_r[1],ps_r[2],ps_r[3],layout=(3,1);ylims = (0, 0.3),title="N(0,1)")
+p_sin_r = plot(ps_sin_r[1],ps_sin_r[2],ps_sin_r[3],layout=(3,1);ylims = (0, 0.3),title="sin(x) + N(0,3)")
+p_sin = plot(ps_sin[1],ps_sin[2],ps_sin[3],layout=(3,1);ylims = (0, 0.3),title="sin(x)")
+p_r = plot(ps_r[1],ps_r[2],ps_r[3],layout=(3,1);ylims = (0, 0.3),title="N(0,3)")
 
-plot(p_sin,p_r,layout=(1,2))
-plot(ps[3],ps_r[3]; title = "Sin(x) vs. N(0,1)")
+plot(p_sin_r,p_sin,p_r,layout=(1,3))
+plot!(size=(1600,1600))
+plot(p_sin_r[3],p_sin[3],p_r[3]; title = "Sin(x) vs. N(0,1)")
 
---
+++ test_paral.jl
@@ -0,0 +1,52 @@
+using Distributed
+using SharedArrays
+#addprocs(2)
+
+@everywhere begin
+        using Pkg;
+        include("src/SymbolicInference.jl") 
+        Pkg.precompile()
+end
+    
+@everywhere begin 
+        using Plots
+
+        my_seed = 1234
+        cycles = 3
+        sd_norm = 1
+        rec_rate = 0.2
+
+        rng = MersenneTwister(my_seed)
+
+        time_series_sin = map(sin,0:0.3:2*pi*cycles)
+        n_sample=length(time_series_sin)
+        dist = Normal(0,sd_norm)
+        time_series = rand(dist,n_sample)
+        time_series_sin_nois = time_series .+ time_series_sin
+end
+
+@everywhere res_recs = pmap(x -> RecurrenceAnalysis.RecurrenceMatrix(x,rec_rate;fixedrate=true),
+                [time_series_sin_nois,time_series_sin,time_series])
+
+
+#res_infs = SharedArray[]
+#@everywhere for i in ["double","poincare","recurrence"]
+#        cur_res = pmap(x -> double_inference(x;seqs = i),res_recs)
+#        push!(res_infs,cur_res)
+#end
+
+res_infs_d = pmap(x -> double_inference(x;seqs = "double"),res_recs)
+res_infs_p = pmap(x -> double_inference(x;seqs = "poincare"),res_recs)
+res_infs_r = pmap(x -> double_inference(x;seqs = "recurrences"),res_recs)
+
+ps_sin_r = map(plot,res_infs_d)
+ps_sin = map(plot,res_infs_p)
+ps_r = map(plot,res_infs_r)
+
+p_sin_r = plot(ps_sin_r[1],ps_sin_r[2],ps_sin_r[3],layout=(3,1);ylims = (0, 0.3),title="Double")
+p_sin = plot(ps_sin[1],ps_sin[2],ps_sin[3],layout=(3,1);ylims = (0, 0.3),title="Poincare")
+p_r = plot(ps_r[1],ps_r[2],ps_r[3],layout=(3,1);ylims = (0, 0.3),title="Recurrence")
+
+plot(p_sin_r,p_sin,p_r,layout=(1,3),size=(1600,1600))
+plot(p_sin_r[3],p_sin[3],p_r[3]; title = "Sin(x) vs. N(0,1)")
+
