56bcd298b8119386a595072ce6cb81fbe632d1b2
DESKTOP-1PNMURG\Filipe <filipe.cruz.ribeiro@gmail.com>
2024-07-02 16:00:04 -0300

    moving data load to its own file and performing analysis with dengue data

--- test/analise_dengue.jl
+++ test/analise_dengue.jl
@@ -3,61 +3,24 @@ using DataFrames
 using SymbolicInference
 using RecurrenceAnalysis
 using Normalization
+include("data_load.jl")
 
-function extract_recurrences_v2(data_source::Vector{Float64}, 
-    motifs_dict::Dict{String, Vector{Any}}; num_windows::Int64 = 3)
-    
-    probs_n = first(size(motifs_dict["Probs"][1]))
-    probs_flat = vcat(motifs_dict["Probs"]...)
-    sorted_probs_inds = sortperm(probs_flat)
+rec_rate = 0.3
 
-    motifs_flat = vcat(motifs_dict["Motifs starts and duration"]...)
-    motifs_by_window = motifs_flat[sorted_probs_inds[1:num_windows]]
+normal_rec_matrix = RecurrenceAnalysis.RecurrenceMatrix(ts_raw_d, rec_rate; fixedrate=true)
+ma_rec_matrix = RecurrenceAnalysis.RecurrenceMatrix(ts_ma_d, rec_rate; fixedrate=true)
 
-    full_data_dict = []
-    for (index, motif) in enumerate(motifs_by_window)
-        motif_start, motif_size = motif
-        motif_range = motif_start:(motif_start+motif_size)-1
-        motif_y1 = data_source[motif_range]
-        
-        
-        motif_window = div(sorted_probs_inds[index],probs_n)
+# short range 
+motifs_dict_normal = SymbolicInference.rec_matrix_motifs(normal_rec_matrix; window_range=collect(1:3), n_motifs=3)
+motifs_dict_ma = SymbolicInference.rec_matrix_motifs(ma_rec_matrix; window_range=collect(1:3), n_motifs=3)
 
-        motif_rec_start = motif_start + motif_window
-        motif_rec_range = motif_rec_start:(motif_rec_start+motif_size)-1 
-        motif_y2 = data_source[motif_rec_range]
-        
-        push!(full_data_dict, Dict(
-        "x1" => collect(motif_range), "y1" => motif_y1,
-        "x2" => collect(motif_rec_range), "y2" => motif_y2,
-        "window" => motif_window, "size" => motif_size,
-        "prob" => probs_flat[sorted_probs_inds][index]))
-    end
-    return full_data_dict
-end
 
-rec_rate = 0.4;
+# long range 
+motifs_dict_normal = SymbolicInference.rec_matrix_motifs(normal_rec_matrix; window_range=collect(1:12), n_motifs=2)
+motifs_dict_ma = SymbolicInference.rec_matrix_motifs(ma_rec_matrix; window_range=collect(1:12), n_motifs=2)
 
-df = CSV.read("data/sanjuan_data.csv", DataFrame)
-ts_raw = df[!, "total_cases"]
-ts_ma = df[!, "mean_cases"]
+coordinate_normal = SymbolicInference.extract_recurrences(ts_raw_d, motifs_dict_normal; num_windows=12)
+coordinate_ma = SymbolicInference.extract_recurrences(ts_ma_d, motifs_dict_ma; num_windows=12)
 
-ts_raw = convert(Vector{Float64}, ts_raw)
-ts_ma = convert(Vector{Float64}, ts_ma[3:end])
-
-norm_fit = fit(UnitEnergy, ts_raw)
-ts_raw_unit = normalize(ts_raw, norm_fit)
-
-norm_fit_ma = fit(UnitEnergy, ts_ma)
-ts_ma_unit = normalize(ts_ma, norm_fit_ma)
-
-normal_rec_matrix = RecurrenceAnalysis.RecurrenceMatrix(ts_raw, rec_rate; fixedrate=true)
-ma_rec_matrix = RecurrenceAnalysis.RecurrenceMatrix(ts_ma, rec_rate; fixedrate=true)
-
-motifs_dict_normal = SymbolicInference.rec_matrix_motifs(normal_rec_matrix; max_window=120, n_motifs=3)
-motifs_dict_ma = SymbolicInference.rec_matrix_motifs(ma_rec_matrix; max_window=120, n_motifs=3)
-
-coordinate_normal = extract_recurrences_v2(ts_raw, motifs_dict_normal; num_windows=120)
-coordinate_ma = extract_recurrences_v2(ts_ma, motifs_dict_ma; num_windows=120)
-p = SymbolicInference.plot_motifs(ts_raw, coordinate_normal; n_motifs=4)
-p2 = SymbolicInference.plot_motifs(ts_ma, coordinate_ma; n_motifs=6)
+p = SymbolicInference.plot_motifs(ts_raw_d, coordinate_normal; n_motifs=24)
+p2 = SymbolicInference.plot_motifs(ts_ma_d, coordinate_ma; n_motifs=24)
--- test/analise_dengue_multi.jl
+++ test/analise_dengue_multi.jl
@@ -7,46 +7,7 @@ using CairoMakie
 
 rec_rate = 0.3;
 
-df = CSV.read("data/sanjuan_data.csv", DataFrame)
-ts_raw_p = df[!, "precipitation"]
-ts_ma_p = df[!, "mean_precip"]
-
-#  Precipitation
-
-ts_raw_p = convert(Vector{Float64}, ts_raw_p)
-ts_ma_p = convert(Vector{Float64}, ts_ma_p[3:end])
-
-norm_fit_p = fit(UnitEnergy, ts_raw_p)
-ts_raw_unit_p = normalize(ts_raw_p, norm_fit_p)
-
-norm_max_fit_p = fit(MinMax, ts_raw_p)
-ts_raw_max_p = normalize(ts_raw_p, norm_max_fit_p)
-
-norm_fit_ma_p = fit(UnitEnergy, ts_ma_p)
-ts_ma_unit_p = normalize(ts_ma_p, norm_fit_ma_p)
-
-norm_max_fit_ma_p = fit(MinMax, ts_ma_p)
-ts_ma_max_p = normalize(ts_ma_p, norm_max_fit_ma_p)
-
-# Dengue
-
-ts_raw_d = df[!, "total_cases"]
-ts_ma_d = df[!, "mean_cases"]
-
-ts_raw_d = convert(Vector{Float64}, ts_raw_d)
-ts_ma_d = convert(Vector{Float64}, ts_ma_d[3:end])
-
-norm_fit_d = fit(UnitEnergy, ts_raw_d)
-ts_raw_unit_d = normalize(ts_raw_d, norm_fit_d)
-
-norm_max_fit_d = fit(MinMax, ts_raw_d)
-ts_raw_max_d = normalize(ts_raw_d, norm_max_fit_d)
-
-norm_fit_ma_d = fit(UnitEnergy, ts_ma_d)
-ts_ma_unit_d = normalize(ts_ma_d, norm_fit_ma_d)
-
-norm_max_fit_ma_d = fit(MinMax, ts_ma_d)
-ts_ma_max_d = normalize(ts_ma_d, norm_max_fit_ma_d)
+include("data_load.jl")
 
 #
 persistence_motifs(ts_ma_max_d;n_windows=50)
